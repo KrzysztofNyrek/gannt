e41901b4d895c4cc9f794c28ff6ec00b1c23f321
Krzysiek <krzysztof.nyrek@gmail.com>
2020-09-25 18:49:48 +0200

    Add calculation set pointer in the end of task box

--- js/oprogramowanie.js
+++ js/oprogramowanie.js
@@ -1,6 +1,8 @@
 //zmienne
 var iloscOsobProjekt = 1;
 var iloscZadan = 1;
+var leftValue = 20;
+var leftWskaznik = 12;
 
 //położenie wskaźnika ścieżki
 document.getElementById('sciezki').onchange = pozycjaSciezki;
@@ -34,23 +36,10 @@ function dodajZadanie(){
         var zadanie = document.getElementById('zadanie').value;
         var start = document.getElementById('wskaznikStartu');
         var iloscDni = document.getElementById('czas').value;
-
-        //var wskaznik = document.getElementById('wskaznikStartu');
         var topValue = 20;
-        var leftValue = 20;
-        var leftWskaznik = 12;
         var dlugoscCegielki = 140;
-
-        //liczenie pozycji startowej kolejnej cegiełki
-        if (iloscZadan > 1){
-            leftValue = 140 * (iloscZadan -1) + 20 + iloscZadan;
-        };
     
-        //liczenie pozycji wskaźnika
-        if (iloscZadan >1){
-            leftWskaznik = leftValue - leftWskaznik + 5;
-        }
-            //liczenie pozycji wskaźnika startu
+        //liczenie pozycji wskaźnika startu
         if (sciezka > 1){
             topValue = 20 + (sciezka -1) * 80;
         };
@@ -65,24 +54,24 @@ function dodajZadanie(){
         else{
             //usuwanie kanwy zadań
             start.remove();
-
+            console.log('przed obliczeniami',leftValue, leftWskaznik);
+            
             //doklejanie kodu HTML kolejnych cegiełek zadań
             zadanie = zadanie.replace(/ /g, "&nbsp");
             kanwaZadan.innerHTML += `<div id="cegielka" style="top: ${topValue}px; left: ${leftValue}px; width: ${dlugoscCegielki}px;"><p style="text-align: center;">${zadanie}</p></div>`;
+            
+            //liczenie pozycji startowej kolejnej cegiełki
+            leftValue += dlugoscCegielki;
 
-                
+            //liczenie pozycji wskaźnika
+            leftWskaznik = leftValue - 6;
             //operacje na wskaźniku startu
             kanwaZadan.innerHTML += `<div id="wskaznikStartu" style="left:${leftWskaznik}px;"></div>`;
 
-            iloscZadan += 1;
-            }
-
-            return(iloscZadan);
+            console.log(leftValue, leftWskaznik);
+            return(leftValue, leftWskaznik);
         }
-        
-    
-
-    
+    }  
 }
 function pozycjaSciezki(){
 
